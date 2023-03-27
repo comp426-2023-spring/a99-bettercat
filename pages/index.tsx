a8f7d5993e299f18fa316586aff1ee1d4e398c57
Ajay Gandecha <ajgandecha@gmail.com>
2023-03-26 23:45:53 -0400

    Initialize Firebase Firestore Database
    This commit initializes the Firebase Firestore database and connects the database to the application.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import { initFirebase } from '@/firebase/clientApp'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 import Head from 'next/head'
 
 const app = initFirebase();
-console.log(app);
-
 const auth = getAuth();
 
 export default function Home() {
 
   const [user, loading, error] = useAuthState(auth);
+
+  const [restaurants, restaurantsLoading, restaurantsError] = useCollection(
+    collection(getFirestore(app), "restaurants")
+  );
 
   if (error) {
     return (
@@ -28,6 +32,9 @@ export default function Home() {
       <div>
         <p>Signed In User: {user.email}</p>
         <button onClick={() => auth.signOut()}>Sign out</button>
+        <h1>Restaurants</h1>
+        {restaurants?.docs.map((doc) =>
+        <p>{doc.get("name")}</p>)}
       </div>
     );
   }
