67f529a9c5dea48661168f8ab81e69833c9d688c
Zhang Weiye <zhangwy@ad.unc.edu>
2023-03-28 17:48:48 -0400

    added packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
-import { initFirebase } from '@/firebase/clientApp'
+import { initFirebase } from "@/firebase/clientApp";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 
-import Head from 'next/head'
+import Head from "next/head";
 
 const app = initFirebase();
 const auth = getAuth();
 
 export default function Home() {
-
   const [user, loading, error] = useAuthState(auth);
 
-  const [restaurants, restaurantsLoading, restaurantsError] = useCollection(
-    collection(getFirestore(app), "restaurants")
-  );
+  const [restaurants, restaurantsLoading, restaurantsError] = useCollection(collection(getFirestore(app), "restaurants"));
 
   if (error) {
     return (
@@ -33,8 +30,9 @@ export default function Home() {
         <p>Signed In User: {user.email}</p>
         <button onClick={() => auth.signOut()}>Sign out</button>
         <h1>Restaurants</h1>
-        {restaurants?.docs.map((doc) =>
-        <p>{doc.get("name")}</p>)}
+        {restaurants?.docs.map((doc) => (
+          <p>{doc.get("name")}</p>
+        ))}
       </div>
     );
   }
@@ -48,4 +46,4 @@ export default function Home() {
 const authenticate = () => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider);
-}
+};
