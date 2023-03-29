a438deb0f5e8e20ff3226f195feafb86f476e044
Zhang Weiye <zhangwy@ad.unc.edu>
2023-03-28 20:52:42 -0400

    pulled example code from Repo

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,41 +1,37 @@
 import Link from "next/link";
 import { useState } from "react";
 
-
 export default function Example() {
+  let value = "value";
+  const li = [1, 2, 3, 4];
 
-    let value = "value";
-    const li = [1,2,3,4];
+  //let i = 0;
+  const [/* State variable */ i, /* Updating function */ setI] = useState(/* Initial value */ 0);
 
-    //let i = 0;
-    const [/* State variable */ i, /* Updating function */ setI] = useState(/* Initial value */ 0);
+  return (
+    <div>
+      <p>Hello!</p>
+      <p>This is a {value} in a HTML element</p>
 
-    return(
-        <div>
-            <p>Hello!</p>
-            <p>This is a {value} in a HTML element</p>
-            
-            {/* This is a comment */}
-            
-            {/* This is a loop over the items */}
-            { li.map((element) => 
-                <p>This is an {element}</p>
-            )}
+      {/* This is a comment */}
 
-            <Link href={"/"}>This is a link</Link>
+      {/* This is a loop over the items */}
+      {li.map((element) => (
+        <p>This is an {element}</p>
+      ))}
 
-            <p className=" text-center font-bold">This is some styled text!</p>
+      <Link href={"/"}>This is a link</Link>
 
-            {/* This is a state: */}
+      <p className="text-center font-bold">This is some styled text!</p>
 
-            <p>{i}</p>
-            <button onClick={increment}>
-                Increase Number
-            </button>
-        </div>
-    );
+      {/* This is a state: */}
 
-    function increment() {
-        setI(i + 1);
-    }
+      <p>{i}</p>
+      <button onClick={increment}>Increase Number</button>
+    </div>
+  );
+
+  function increment() {
+    setI(i + 1);
+  }
 }
