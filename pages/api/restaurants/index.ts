8c12c598c98f4ff129ac0257667561c206fec6af
Ajay Gandecha <ajgandecha@gmail.com>
2023-04-09 15:16:42 -0400

    Add Restaurants API routes

---
+++ pages/api/restaurants/index.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as DataService from '@/lib/DataService'
+
+/**
+ * API Route: /api/restaurants/
+ * API handler to fetch all restaurants data.
+ */
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    
+    // Fetch data
+    const restaurants = await DataService.getAllRestaurants();
+
+    // Return courses data
+    res.status(200).json(restaurants);
+  }
+  // Handle any errors here
+  catch(err) {
+    res.status(500).send({ error: "Failed to fetch data"});
+  }
+}
