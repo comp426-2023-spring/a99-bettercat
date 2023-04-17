1e94bba41d2f2589b36e07d014fdd742f391e2eb
wastilla <wastilla@email.unc.edu>
2023-04-17 16:08:42 -0400

    Set up first few sections of page to be populated with data

--- pages/restaurants/[id].tsx
+++ pages/restaurants/[id].tsx
@@ -27,28 +27,30 @@ export default function RestaurantView({restaurant, reviews, average_review}: Re
 
    // Render page
    return(
-     <div className="flex flex-col bg-slate-300 min-h-screen p-10">
+     <div className="flex flex-col bg-slate-300 min-h-screen p-10 space-y-5">
          <h3 className ="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{restaurant.name}</h3>
          <h3 className = "mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{restaurant.description}</h3>
-         <div className="content-center">
+         <div className="self-center">
             <a href={restaurant.website} className="inline-flex items-center justify-center  px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Website
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
         </div>
-        <div>
-            <h3>{reviews.length} review(s)</h3>
-            <h3>{average_review.toString()+ "/5 stars"}</h3>
-        </div>
-        
-         
-        
+        <div className="flex flex-row pd- 10 space-x-5">
+            <div className="bg-white rounded-lg p-10 drop-shadow-lg grow">
+                <h1 className="font-extrabold text-3xl">General Info</h1>
+            </div>
+            <div className="bg-white rounded-lg p-10 drop-shadow-lg grow">
+                <h1 className="font-extrabold text-3xl">Map</h1>
+            </div>
+        </div> 
         <div className="bg-white rounded-lg p-10 drop-shadow-lg">
+            <h1 className="font-extrabold text-3xl">Reviews</h1>
             <div className= "flex flex-row">
             {reviews?.map((reviews) =>
             <>
-                <div className="p-10 rounded-lg divide-gray-600">
-                    <p><strong>{reviews.score}/5</strong></p>
+                <div className="p-10 rounded-lg bg-slate-400">
+                    <p className="align-middle"><strong>{reviews.score}/5</strong></p>
                     <p>{reviews.text}</p>
                 </div>
                 
@@ -56,6 +58,10 @@ export default function RestaurantView({restaurant, reviews, average_review}: Re
             }
             </div>
          </div>
+         <div>
+            <h3>{reviews.length} review(s)</h3>
+            <h3>{average_review.toString()+ "/5 stars"}</h3>
+        </div>
          
        </div>
    );
