766e9ed1c610208cfa45a507ba1a2af02d8351fe
Andrew Zadrozny <andrewza@email.unc.edu>
2023-04-17 21:27:46 -0400

    added template to restaurant list

--- components/Restaurant_card.tsx
+++ components/Restaurant_card.tsx
@@ -8,8 +8,8 @@ export default function Restaurant_card({ restaurant }: ResCardProps) {
   console.log(restaurant);
   const priceSymbol = "$".repeat(restaurant.price);
   return (
-    <div className="rounded-2xl border border-black p-6 flex flex-col gap-4">
-      <p className="text-2xl font-bold ">{restaurant.name}</p>
+    <div className="bg-white rounded-2xl border border-black p-6 flex flex-col gap-4">
+      <p className="text-2xl font-extrabold ">{restaurant.name}</p>
       <p className="text-sm">{restaurant.description}</p>
       <div className="flex flex-wrap justify-between gap-2">
         <div className="flex justify-between gap-2">
--- pages/restaurants.tsx
+++ pages/restaurants.tsx
@@ -4,7 +4,9 @@ interface HomeProps {
 
 export default function Restaurants({ restaurants }: HomeProps) {
   return (
-    <div className="flex flex-col mx-24 my-1 gap-4">
+    <div className="flex flex-col bg-slate-300 min-h-screen p-10 space-y-5">
+      <h1 className ="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Chapel Hill Restaurants</h1>
+      <Restaurant_card restaurant={restaurants[0]} />
       <Restaurant_card restaurant={restaurants[0]} />
       <Restaurant_card restaurant={restaurants[0]} />
     </div>
