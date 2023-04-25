e859808673ff98c3a86ec9e07498a1a271ba0050
Weiye (David) Zhang <97468260+zhangwy324@users.noreply.github.com>
2023-04-25 19:13:59 -0400

    Restaurant List Page (#21)
    * added packages
    
    * pulled example code from Repo
    
    * Add Restaurant, Review, and User Data Models
    
    This commit defines the `Restaurant`, `Review`, and `User` data models which will be used to specify the structure of data retrieved from and posted to the Firebase Firestore database.
    
    * Update Error with Review Tags List
    
    Fixes a small typo in the typing of `tags` from `[string]` to `string[]` to follow TypeScript convention.
    
    * Rename Modules Directory to Models
    
    * Create DataService
    
    This commit creates the DataService to connect to the Firebase Firestore database.
    
    The DataService provides helper methods that access data.
    
    This commit also modifies `index.tsx` to use the data from the service.
    
    * added restaurant page, and restaurant-card component
    
    * add comment for no access to restaurant id
    
    * moved page to restaurants folder
    
    * fixed merged conflict from dev branch
    
    * fixed merge conflict from dev branch
    
    * removed debugging log
    
    * added template to restaurant list
    
    * now maps through all restaurants
    
    * now shows the hour of the current weekday
    
    * loaded environment variable and installed packages required for google maps api
    
    * formatted hours text
    
    * changed to RestaurantCard
    
    * fixed component name
    
    ---------
    
    Co-authored-by: Ajay Gandecha <ajgandecha@gmail.com>
    Co-authored-by: Andrew Zadrozny <andrewza@email.unc.edu>

---
+++ components/RestaurantCard.tsx
@@ -0,0 +1,61 @@
+import Restaurant from "@/models/Restaurant";
+import Link from "next/link";
+
+interface ResCardProps {
+  restaurant: Restaurant;
+}
+
+export default function RestaurantCard({ restaurant }: ResCardProps) {
+  const weekDay = getWeekday();
+  const priceSymbol = "$".repeat(restaurant.price);
+  return (
+    <div className="bg-white rounded-2xl border border-black p-6 flex flex-col gap-4">
+      <Link
+        href={`/restaurants/${restaurant.id}`} // TODO: currently no access to id
+        className="text-2xl font-extrabold "
+      >
+        {restaurant.name}
+      </Link>
+      <p className="text-sm">{restaurant.description}</p>
+      <div className="flex flex-wrap justify-between gap-2">
+        <div className="flex justify-between gap-2">
+          <p>{restaurant.category}</p>
+          <p>{priceSymbol}</p>
+        </div>
+        {/* Display the hours of current weekday */}
+        <p>
+          Hours {weekDay.charAt(0).toLocaleUpperCase() + weekDay.slice(1)}: {Math.floor(restaurant.hours[weekDay].open / 100).toString()}:{restaurant.hours[weekDay].open.toString().slice(2)} - {Math.floor(restaurant.hours[weekDay].close / 100).toString()}:
+          {restaurant.hours[weekDay].close.toString().slice(2)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+// Helper method that get the current week day
+function getWeekday() {
+  const day = new Date().getDay();
+  switch (day) {
+    case 0:
+      return "sunday";
+      break;
+    case 1:
+      return "monday";
+      break;
+    case 2:
+      return "tuesday";
+      break;
+    case 3:
+      return "wednesday";
+      break;
+    case 4:
+      return "thursday";
+      break;
+    case 5:
+      return "friday";
+      break;
+    default:
+      return "saturday";
+      break;
+  }
+}
