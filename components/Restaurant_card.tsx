e64c53d89e39383e5412548399784afaabe23599
Zhang Weiye <zhangwy@ad.unc.edu>
2023-04-25 17:25:56 -0400

    now shows the hour of the current weekday

--- components/Restaurant_card.tsx
+++ components/Restaurant_card.tsx
@@ -6,6 +6,7 @@ interface ResCardProps {
 }
 
 export default function Restaurant_card({ restaurant }: ResCardProps) {
+  const weekDay = getWeekday();
   const priceSymbol = "$".repeat(restaurant.price);
   return (
     <div className="bg-white rounded-2xl border border-black p-6 flex flex-col gap-4">
@@ -22,9 +23,37 @@ export default function Restaurant_card({ restaurant }: ResCardProps) {
           <p>{priceSymbol}</p>
         </div>
         <p>
-          Hours: {restaurant.hours.monday.open} - {restaurant.hours.monday.close}
+          Hours Today: {Math.floor(restaurant.hours[weekDay].open / 100).toString()}:{restaurant.hours[weekDay].open.toString().slice(2)} - {Math.floor(restaurant.hours[weekDay].close / 100).toString()}:{restaurant.hours[weekDay].close.toString().slice(2)}
         </p>
       </div>
     </div>
   );
+}
+
+// Helper method that get the current week day
+function getWeekday() {
+  const day = new Date().getDay();
+  switch (day) {
+    case 0:
+      return "sunday";
+      break;
+    case 1:
+      return "monday";
+      break;
+    case 2:
+      return "tuesday";
+      break;
+    case 3:
+      return "wednesday";
+      break;
+    case 4:
+      return "thursday";
+      break;
+    case 5:
+      return "friday";
+      break;
+    default:
+      return "saturday";
+      break;
+  }
 }
