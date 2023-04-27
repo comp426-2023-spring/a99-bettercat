af281d48e70d4dbfb7417d72ccffc26135b6e990
Andrew Zadrozny <andrewza@email.unc.edu>
2023-04-26 23:49:09 -0400

    home page
    modified home page
    redirects to restaurant list on sign in
    redirects back to home page when sign out

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,26 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Head from "next/head";
 
 const app = initFirebase();
 const auth = getAuth();
 
-/** Interface for the parameter of the `Home` component. */
 interface HomeProps {
   restaurants: Restaurant[];
 }
 export default function Home({ restaurants }: HomeProps) {
   const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/restaurants"); 
+    }
+  }, [user]);
 
   if (error) {
     return (
@@ -28,7 +36,7 @@ export default function Home({ restaurants }: HomeProps) {
   }
   if (user) {
     return (
-      <div>
+      <div className="flex flex-col bg-slate-300 min-h-screen p-10 space-y-5">
         <p>Signed In User: {user.email}</p>
         <button onClick={() => auth.signOut()}>Sign out</button>
         <h1>Restaurants</h1>
@@ -39,8 +47,17 @@ export default function Home({ restaurants }: HomeProps) {
     );
   }
   return (
-    <div className="App">
-      <button onClick={() => authenticate()}>Sign In</button>
+    <div className="flex flex-col items-center justify-center bg-slate-300 min-h-screen">
+      <h1 className="text-4xl font-bold mb-5">Welcome to Chapel Hill Eats</h1>
+      <p className="text-lg mb-10">Sign in to get started:</p>
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={() => authenticate()}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
--- pages/restaurants.tsx
+++ pages/restaurants.tsx
@@ -1,14 +1,30 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useRouter } from "next/router";
+const auth = getAuth();
+
 interface HomeProps {
   restaurants: Restaurant[];
 }
 
 export default function Restaurants({ restaurants }: HomeProps) {
+
+  const router = useRouter(); 
+
+  const handleSignOut = () => {
+    auth.signOut();
+    router.push("/");
+  };
+
+
+
+
   return (
     <div className="flex flex-col bg-slate-300 min-h-screen p-10 space-y-5">
       <h1 className ="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Chapel Hill Restaurants</h1>
       <Restaurant_card restaurant={restaurants[0]} />
       <Restaurant_card restaurant={restaurants[0]} />
       <Restaurant_card restaurant={restaurants[0]} />
+      <button onClick={handleSignOut}>Sign out</button>
     </div>
   );
 }
