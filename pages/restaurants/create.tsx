e58af2b3efce64481dce94d4b24766b926451f1c
Zhang Weiye <zhangwy@ad.unc.edu>
2023-04-27 19:10:26 -0400

    set up /create page

---
+++ pages/restaurants/create.tsx
@@ -0,0 +1,115 @@
+import { initFirebase } from "@/firebase/clientApp";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+import Head from "next/head";
+
+import RestaurantCard from "@/components/RestaurantCard";
+import * as DataService from "@/lib/DataService";
+import Restaurant, { RestaurantHours, RestaurantHoursSchedule } from "@/models/Restaurant";
+import { useState } from "react";
+
+const app = initFirebase();
+const auth = getAuth();
+
+interface RestaurantFormInfo {
+  name: string; // Name of the restaurant
+  category: string; // Category of the restaurant (Mediterranean, Indian, etc.)
+  address: string; // String representation of restaurant's location
+  phone: string; // Phone number used by the restaurant
+  website: string; // Website used by the restaurant
+  description: string; // Description of the restaurant
+  hours: RestaurantHoursSchedule; // Describes the restaurant's hours of operation
+  price: number; // Representation of average cost ( 1 = $, 2 = $$, 3 = $$$, 4 = $$$$)
+}
+
+const initRestaurantHour: RestaurantHours = {
+  open: 900,
+  close: 2200,
+};
+
+export default function Restaurants() {
+  /* Load user authentication hook
+   - user: Once authenticated user loads, user !== null.
+   - loading: If authenticated state is loading, loading !== null.
+   - error: If there was an error loading authentication, error !== null.
+   > If all the above == null, then it means that no user is currently signed in.
+   */
+  const [user, loading, error] = useAuthState(auth);
+
+  const [restaurantInfo, setRestaurantInfo] = useState({
+    name: "", // Name of the restaurant
+    category: "", // Category of the restaurant (Mediterranean, Indian, etc.)
+    address: "", // String representation of restaurant's location
+    phone: "", // Phone number used by the restaurant
+    website: "", // Website used by the restaurant
+    description: "", // Description of the restaurant
+    hours: {
+      sunday: initRestaurantHour,
+      monday: initRestaurantHour,
+      tuesday: initRestaurantHour,
+      wednesday: initRestaurantHour,
+      thursday: initRestaurantHour,
+      friday: initRestaurantHour,
+      saturday: initRestaurantHour,
+    } as RestaurantHoursSchedule, // Describes the restaurant's hours of operation
+    price: 2, // Representation of average cost ( 1 = $, 2 = $$, 3 = $$$, 4 = $$$$)
+  } as RestaurantFormInfo);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setRestaurantInfo((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+  // Render page
+  return (
+    <div className="flex flex-col bg-slate-300 min-h-screen p-10 space-y-5">
+      <h1 className="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Add Restaurant</h1>
+
+      <div>
+        <div>
+          <label>Restaurant Name:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={restaurantInfo.name}
+          />
+        </div>
+        <div>
+          <label>Restaurant Category:</label>
+          <input
+            type="text"
+            name="category"
+            onChange={handleChange}
+            value={restaurantInfo.category}
+          />
+        </div>
+        <div>
+          <label>Restaurant Address:</label>
+          <input
+            type="text"
+            name="address"
+            onChange={handleChange}
+            value={restaurantInfo.address}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Helper method that allows authentication with the Firebase Google auth provider.
+ */
+const authenticate = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
