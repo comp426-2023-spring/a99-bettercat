19124a0079142ce11067fefd5fd06e0522ba8c8f
Ajay Gandecha <ajgandecha@gmail.com>
2023-05-05 15:08:34 -0400

    Add Write Reviews Feature

--- pages/restaurants/create-review/[id].tsx
+++ pages/restaurants/create-review/[id].tsx
@@ -5,6 +5,12 @@ import { getFirestore, collection } from "firebase/firestore";
 import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
+import * as DataService from "@/lib/DataService";
+import Review from "@/models/Review";
+import { GetStaticPaths, GetStaticPropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/router";
 
 const app = initFirebase();
 const auth = getAuth();
@@ -30,7 +36,6 @@ export default function CreateReview({ restaurant }: CreateReviewProps) {
   const [user, loading, error] = useAuthState(auth);
 
   const [reviewInfo, setReviewInfo] = useState({
-    userId: auth.currentUser?.uid,// id of review author
     restaurantId: restaurant.id, // id of restaurant 
     score: 0, // score (out of five) user leaves for restaurant
     tags: [""], // tags for review
@@ -58,17 +63,29 @@ export default function CreateReview({ restaurant }: CreateReviewProps) {
         }
     })
   }
+
+  const router = useRouter();
   async function handleSubmit() {
     let data = reviewInfo;
     console.log(data.score)
     console.log(data.text)
-    //insert backend method
+    let review: ReviewFormInfo = {
+      userId: user!.uid,
+      restaurantId: data.restaurantId,
+      score: data.score,
+      tags: data.tags,
+      text: data.text
+    }
+    await DataService.createReview(review as unknown as Review)
+
+    router.push("/restaurants/" + data.restaurantId);
   }
 
   return(
     <div className="flex flex-col bg-slate-300 sm:p-8 space-y-5 h-screen">
         <div className="p-2">
             <h1 className="mb-2 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Leave a Review</h1>
+            <h2 className="block text-xl text-center pb-2">{restaurant.name}</h2>
             <div className="border border-black flex flex-col items-center gap-2 bg-white rounded-3xl p-4 ">
             <div className="flex flex-col w-80 sm:w-96 md:w-[500px] lg:w-[750px] space-y-5">
             <h2 className="block text-2xl">Please Rate Your Experience:</h2>
@@ -128,5 +145,35 @@ export default function CreateReview({ restaurant }: CreateReviewProps) {
     </div>
   );
 
+}
 
+/**
+ * Returns that paths that have to be created at build time.
+ * @returns static paths.
+ */
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  // Return such that no pages need to be created at build time,
+  // and a "blocking" fallback.
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+interface RestaurantParams extends ParsedUrlQuery {
+  slug: string;
+}
+/**
+ * Loads data on the server side as the page loads.
+ * @returns properties for the `index` page with the data loaded.
+ */
+export async function getStaticProps(context: GetStaticPropsContext) {
+  // Get router and ID from the URL.
+  const { id } = context.params as RestaurantParams;
+
+  // Get restaurant data
+  const restaurant = await DataService.getRestaurant(Array.isArray(id) ? id[0] : id!);
+
+
+  return { props: { restaurant: restaurant} };
 }
