418d66f553e146469783581a5caedee2eff64064
Weiye (David) Zhang <97468260+zhangwy324@users.noreply.github.com>
2023-05-05 19:02:17 -0400

    made navbar responsive (#44)

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -1,42 +1,50 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 import { auth } from "@/firebase/clientApp";
-import unc from "public/unc.png"
-import { useAuthState } from 'react-firebase-hooks/auth';
+import unc from "public/unc.png";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 // interface navBarProps {
 //   user_id: String | null;
 // }
 // { user_id }:navBarProps
-export default function NavBar(){
-
+export default function NavBar() {
   const [user, loading, error] = useAuthState(auth);
 
-    return (
-        <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-30 w-full">    
-            <div className="flex items-center justify-center w-full"> {/*logo container*/}    
-                <Link className="text-xl font-semibold px-20" href="/">
-                  <Image
-                    src ={unc}
-                    height={150}
-                    width={150}
-                    alt = "Unc" 
-                  />
-                  </Link>
-                  <div className="flex flex-row ml-4 space-x-20">
-                  <Link className="text-xl text-blue-300 font-medium my-4" href="/restaurants">View All</Link>
-                  { user && (
-                    <div className="text-xl text-blue-300 font-medium my-4 space-x-20">
-                      <Link href={"/users/" + user.uid} >My Profile</Link>
-                      <button onClick={() => auth.signOut()}>Sign out</button>
-                    </div>
-                    
-                  )}
-                  </div>
-            </div>   
-      </nav>
-    );
-}  
+  return (
+    <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-30 w-full">
+      <div className="flex flex-col lg:flex-row items-center justify-center w-full">
+        {" "}
+        {/*logo container*/}
+        <Link
+          className="text-xl font-semibold px-20"
+          href="/"
+        >
+          <Image
+            src={unc}
+            height={150}
+            width={150}
+            alt="Unc"
+          />
+        </Link>
+        <div className="flex flex-row ml-4 space-x-20">
+          <Link
+            className="text-xl text-blue-300 font-medium my-4"
+            href="/restaurants"
+          >
+            View All
+          </Link>
+          {user && (
+            <div className="text-xl text-blue-300 font-medium my-4 space-x-20">
+              <Link href={"/users/" + user.uid}>My Profile</Link>
+              <button onClick={() => auth.signOut()}>Sign out</button>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 // const findWidth = () => {
 //   const [width, setWidth] = useState(window.innerWidth);
@@ -47,5 +55,3 @@ export default function NavBar(){
 //   }, [width]);
 //   return width;
 // };
-
-  
